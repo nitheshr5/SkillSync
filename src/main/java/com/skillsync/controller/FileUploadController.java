fe94a2a4bebce412404349ae280f4d1afea0e6c6
nitheshr5 <nitheshrpoojari5@gmai.com>
2025-07-25 22:11:51 +0530

    file uploader

--- src/main/java/com/skillsync/controller/FileUploadController.java
+++ src/main/java/com/skillsync/controller/FileUploadController.java
@@ -1,12 +1,18 @@
 package com.skillsync.controller;
 
-import com.skillsync.model.FileUpload;
-import com.skillsync.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.skillsync.model.FileUpload;
+import com.skillsync.service.FileUploadService;
 
 @RestController
 @RequestMapping("/api/files")
